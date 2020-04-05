8050ed066de275d424d1d8a935f7f3671957920b
Luka Žohar <luka.zohi@gmail.com>
2020-04-05 20:00:15 +0200

    Frontend: Updating material imports.

--- apps/snippeter/src/app/modules/material/material.module.ts
+++ apps/snippeter/src/app/modules/material/material.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
@@ -6,25 +7,49 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { TextFieldModule } from '@angular/cdk/text-field';
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatSnackBarModule,
+    MatButtonToggleModule,
+    TextFieldModule
   ],
   exports: [
+    BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatSnackBarModule,
+    MatButtonToggleModule,
+    TextFieldModule
   ]
 })
 export class MaterialModule {}
