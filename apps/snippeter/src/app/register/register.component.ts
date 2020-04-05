8dee02f7a6087f33e3196e03a3e332a30d6a3616
Luka Žohar <luka.zohi@gmail.com>
2020-04-05 02:29:01 +0200

    Frontend: Filled register modal.

--- apps/snippeter/src/app/register/register.component.ts
+++ apps/snippeter/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'snippeter-register',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  email: FormControl = new FormControl(null, [
+    Validators.email,
+    Validators.required
+  ]);
+  nickname: FormControl = new FormControl(null, [Validators.required]);
+  firstName: FormControl = new FormControl(null);
+  lastName: FormControl = new FormControl(null);
+  password: FormControl = new FormControl(null, [Validators.required]);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  register(): void {}
 }
