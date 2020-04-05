41313fa868b6fc74c3030edb1f9e383707aeec52
Luka Žohar <luka.zohi@gmail.com>
2020-04-05 02:16:41 +0200

    Frontend: Added register modal. If register modal is closed, login modal also closes.

--- apps/snippeter/src/app/app.module.ts
+++ apps/snippeter/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialModule } from './material.module';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LoginComponent],
+  declarations: [AppComponent, NavbarComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
--- apps/snippeter/src/app/login/login.component.ts
+++ apps/snippeter/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { FormControl, Validators } from '@angular/forms';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'snippeter-login',
@@ -11,11 +12,22 @@ export class LoginComponent implements OnInit {
   email: FormControl = new FormControl(null);
   password: FormControl = new FormControl(null);
 
-  constructor(public dialogRef: MatDialogRef<LoginComponent>) {}
+  constructor(
+    public dialogRefLogin: MatDialogRef<LoginComponent>,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {}
 
   login(): void {}
 
-  register(): void {}
+  register(): void {
+    const dialogRef = this.dialog.open(RegisterComponent, {
+      width: '250px'
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.dialogRefLogin.close();
+    });
+  }
 }
